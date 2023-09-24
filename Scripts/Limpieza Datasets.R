5aa2ee521d425228fc3712b07ee867ebf3135bc6
Gabriel <gabriel.szerman@gmail.com>
2023-09-24 14:27:13 -0300

    Actualizado Limpieza Datasets para que tome como referencia al path actual desde el path del script

--- Scripts/Limpieza Datasets.R
+++ Scripts/Limpieza Datasets.R
@@ -1,29 +1,42 @@
-require(tidyverse)
+# Obtenemos la ruta del directorio del script actual
+ruta_script <- dirname(rstudioapi::getActiveDocumentContext()$path)
 
-#Bicis
-df_bici = read_csv('Data/trips_2022_reducido.csv') 
-df_bici = df_bici %>%
-  filter( 
-    (duracion_recorrido >= 300) &
-      (duracion_recorrido <= 3600)
-  ) %>%
+# Construimos las rutas a los archivos y directorios relativos
+ruta_data <- file.path(ruta_script, '..', 'Data')
+ruta_entrada <- file.path(ruta_data, 'trips_2022_reducido.csv')
+ruta_salida <- file.path(ruta_data, 'Bicis_limpio.csv')
+
+# Cargamos el archivo de entrada
+df_bici <- read_csv(ruta_entrada)
+
+# Procesamos los datos
+
+# Filtramos los datos para que cumplan con ciertas condiciones en la duración del recorrido
+df_bici <- df_bici %>%
+  filter(duracion_recorrido >= 300 & duracion_recorrido <= 3600)
+
+# Realizamos transformaciones en las columnas
+df_bici <- df_bici %>%
   mutate(
+    # Si el género está ausente, lo reemplazamos con "OTHER"
     Género = if_else(is.na(Género), "OTHER", Género),
+    
+    # Normalizamos y modificamos el formato de los nombres de estación
     nombre_estacion_origen = gsub('\\.','. ',nombre_estacion_origen),
     nombre_estacion_destino = gsub('\\.','. ',nombre_estacion_destino),
     nombre_estacion_origen = str_to_title(nombre_estacion_origen),
     nombre_estacion_destino = str_to_title(nombre_estacion_destino)
-  ) %>%
-  select(!c(X.1, X, id_estacion_origen, direccion_estacion_origen, id_estacion_destino, direccion_estacion_destino))
+  )
 
-#Normalizo los nombres delas columnas
-#veo los inices
-colnames(df_bici)
+# Seleccionamos columnas específicas para mantener
+df_bici <- df_bici %>%
+  select(-X.1, -X, -id_estacion_origen, -direccion_estacion_origen, -id_estacion_destino, -direccion_estacion_destino)
 
-colnames(df_bici)[1] = 'id_recorrido'
-colnames(df_bici)[13] = 'genero'
-colnames(df_bici)
+# Cambiamos al directorio donde se guardará el archivo de salida
+setwd(ruta_data)
 
-write.csv(df_bici, file='Data/Bicis_limpio.csv', row.names = F)
+# Guardamos el archivo procesado
+write.csv(df_bici, file = ruta_salida, row.names = FALSE)
 
-View(read_csv('Data/Bicis_limpio.csv'))
+# Visualizamos el archivo procesado
+View(read_csv(ruta_salida))
