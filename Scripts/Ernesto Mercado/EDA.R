5c7fed6565924fe209adf376ff12de3f5c12f3a1
DocErnstz <ernesto800@outlook.com>
2023-10-04 17:46:38 -0300

    agregado analisis de horario de salida por modelo/dia, por temperatura/dia y  grafico de cantidad de viajes por genero en franja horaria

--- Scripts/Ernesto Mercado/EDA.R
+++ Scripts/Ernesto Mercado/EDA.R
@@ -3,25 +3,41 @@ require(tidyverse)
 df_bici = read_csv('Data/Bicis_limpio.csv')
 View(df_bici)
 
-
-#Que dias son los que la gente mayormente anda en bici?
-df_bici %>%
-  mutate(dia_semana = weekdays(fecha_origen_recorrido)) %>%
-  ggplot(aes(x=dia_semana)) +
-  geom_bar()
-#Los dias de semana suelen tener mayor trafico
-
-#Los hombres conducen mas que las mujeres?
-df_bici %>%
-  ggplot(aes(x=genero)) +
-  geom_bar()
-#clara diferencia entre los dos
-
 df_clima = read_csv('Data/Clima_limpio.csv')
 
+#hora de salida por modelo y dia
+df_bici %>%
+  mutate(hora_de_salida=lubridate::hour(fecha_origen_recorrido)) %>%
+  ggplot(mapping = aes(x=hora_de_salida))+
+  geom_line(stat="count") +
+  scale_x_continuous(breaks = seq(0,23,2)) +
+  facet_wrap(modelo_bicicleta ~ (weekdays(fecha_origen_recorrido)))
 
+#hora de salida por temperatura minima y dia
+df_bici %>%
+  left_join(df_clima) %>%
+  mutate(hora_de_salida=lubridate::hour(fecha_origen_recorrido), aprox_temp = ifelse(temp_min < 10, "cold", ifelse(temp_min >= 10 & temp_min <= 18, "normal", "hot"))) %>%
+  ggplot(mapping = aes(x=hora_de_salida)) +
+  geom_line(stat="count") +
+  scale_x_continuous(breaks = seq(0,23,2))+
+  facet_wrap(~(aprox_temp))
 
-View(df_clima)
+#cantidad de trafico de bicicletas por genero en cada franja horaria
+df_temp1 <- df_bici %>%
+  mutate(franja_horaria = case_when(
+    between(hour(as.POSIXct(fecha_origen_recorrido)), 5, 7) ~ "Franja 1: 5:00 AM - 8:00 AM",
+    between(hour(as.POSIXct(fecha_origen_recorrido)), 8, 10) ~ "Franja 2: 8:00 AM - 11:00 AM",
+    between(hour(as.POSIXct(fecha_origen_recorrido)), 11, 13) ~ "Franja 3: 11:00 AM - 2:00 PM",
+    between(hour(as.POSIXct(fecha_origen_recorrido)), 14, 16) ~ "Franja 4: 2:00 PM - 5:00 PM",
+    between(hour(as.POSIXct(fecha_origen_recorrido)), 17, 19) ~ "Franja 5: 5:00 PM - 8:00 PM",
+    between(hour(as.POSIXct(fecha_origen_recorrido)), 20, 22) ~ "Franja 6: 8:00 PM - 11:00 PM",
+    TRUE ~ "Otras franjas"
+  ))
 
+df_temp1 <- df_temp1 %>%
+  filter(franja_horaria != "Otras franjas")
 
-summary(df_bici)
+df_temp1 %>%
+  ggplot(aes(x=franja_horaria))+
+  geom_bar() +
+  facet_wrap(~genero)
