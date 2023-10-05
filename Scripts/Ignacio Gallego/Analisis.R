ca360bbce0410890aa8099787273c62418b65b97
NachoGalle <102256041+NachoGalle@users.noreply.github.com>
2023-10-05 07:59:55 -0300

    Add files via upload
    agregué viajes diarios en funcion de la temperatura

--- Scripts/Ignacio Gallego/Analisis.R
+++ Scripts/Ignacio Gallego/Analisis.R
@@ -35,6 +35,14 @@ df_bici %>%
   geom_line(stat="count")+
   scale_x_continuous(breaks = seq(0,23,by=2))
 
+df_bici %>%
+  mutate(hora_de_salida=lubridate::hour(fecha_origen_recorrido)) %>%
+  ggplot(mapping = aes(x=hora_de_salida))+
+  geom_bar(stat="count",fill = "#6baed6")+
+  scale_x_continuous(breaks = seq(0,23,by=2))+
+  theme_minimal()+
+  labs(x = "Hora de salida", y = "Cantidad de viajes", 
+       title = "Cantidad de Viajes por Franja Horaria")
 #y mes a mes:
 
 df_bici %>%
@@ -67,3 +75,35 @@ ggplot(df_clima,mapping = aes(x=fecha))+
   annotate("text",label="T Min",x=as.Date("2022-02-01"),y=21.5,color="blue",angle="-45")+
   annotate("text",label="T Prom",x=as.Date("2022-02-10"),y=25,color="grey2",angle="-45")
 
+
+#cantidad de viajes en función de la temperatura
+
+#primero combino ambos datasets según la fecha y selecciono las columnas de temperatura y
+#redondeo los decimales.
+
+df_join <- df_bici %>%
+  inner_join(df_clima,by="fecha") %>%
+  select(temp_prom,temp_max,temp_min) %>%
+  mutate(temp_prom= round(temp_prom),
+         temp_max=round(temp_max),
+         temp_min=round(temp_min))
+
+#ahora grafico la cantidad de viajes según temperatura promedio, máxima, y mínima
+
+ggplot(df_join,mapping=aes(x=temp_prom))+
+  geom_histogram(mapping=aes(x=temp_prom),stat = "count",binwidth = 5,color="black",fill="aquamarine")+
+  labs(title = "Viajes segun temperatura promedio",x="Temperatura (°C)",y="Cantidad de viajes")
+
+ggplot(df_join,mapping=aes(x=temp_max))+
+  geom_histogram(bins=5,stat = "count",color="black",fill="brown1")+
+  labs(title = "Viajes segun temperatura máxima",x="Temperatura (°C)",y="Cantidad de viajes")
+
+ggplot(df_join,mapping=aes(x=temp_min))+
+  geom_histogram(stat = "count",fill="turquoise",color="black")+
+  labs(title = "Viajes segun temperatura mínima",x="Temperatura (°C)",y="Cantidad de viajes")
+
+ggplot(df_join)+
+  geom_boxplot(mapping=aes(y=temp_prom,x="temperatura promedio"),fill="aquamarine")+
+  geom_boxplot(mapping=aes(y=temp_max,x="temperatura maxima"),fill="brown1")+
+  geom_boxplot(mapping=aes(y=temp_min,x="temperatura minima"),fill="turquoise")
+  
